0ce8f091c3838d344ff494794041aa55afed1cb4
william <william_zxs@163.com>
2026-03-01 09:09:24 +0800

    fix: 修复 build 中 vitest/jest-dom 类型错误

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,5 @@
-import { expect } from "vitest";
-import * as matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
 
-expect.extend(matchers);
 afterEach(() => cleanup());
